7299f3a79f2afa2785a5fa86a31e5812dccd1313
bryant-the-coder <foobryant99@gmail.com>
2024-10-13 21:43:27 +0800

    first commit

---
+++ PROBLEMS.md
@@ -0,0 +1,3 @@
+1. create, read, update and delete menu (food names and prices)
+2. create / edit food orders
+
---
+++ restaurant/menu.py
@@ -0,0 +1,53 @@
+import json
+
+
+def line():
+    print("-" * 60)
+
+
+def get_rating(reviews):
+    rating = 5
+    if reviews:
+        rating = sum(reviews) // len(reviews)
+    return "⭐️" * rating
+
+
+with open("menu.json", "r") as f:
+    data = json.load(f)
+
+items = data.get("items", [])
+
+
+line()
+print("Welcome to Nandos")
+line()
+
+while True:
+    print("<1> Menu Maintenance")
+    print("<2> Order Food")
+    print("<3> Check Order")
+    print("<4> Payment")
+    print("<5> Sales Reports")
+    print("<Q>uit")
+    line()
+    choice = int(input("Enter option >> "))
+    print("")
+
+    if choice == 1:
+        food_name = input("Enter item name >> ")
+        price = input("Enter item price >> ")
+        food_type = input("Veg / Non-veg >> ")
+        items.append(
+            {
+                "id": len(items) + 1,
+                "name": food_name,
+                "price": price,
+                "veg": True if food_type == "Non-veg" else False,
+                "reviews": [],
+            }
+        )
+
+        data["items"] = items
+        with open("menu.json", "w") as f:
+            json.dump(data, f)
+            print("item has been added")
